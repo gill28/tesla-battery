7e2894f9186dd73828463bdee815e578ffc50c38
gill28 <cinch@kvonrudnsiphone.cscinch.com>
2020-02-06 15:32:13 -0600

    added tesla car and animated wheels onto the car plus styling

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import TeslaCar from '../components/TeslaCar/TeslaCar';
+import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
 import './TeslaBattery.css';
 class TeslaBattery extends Component {
   render() {
     return (
       <form className='tesla-battery'>
         <h1>Range Per Charge</h1>
+        <TeslaCar wheelsize={19} />
+        <TeslaNotice />
       </form>
     );
   }
